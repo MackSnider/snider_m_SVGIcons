374fc3506057075a9db78c4d4b36cbb1e3137ae8
Mackenzie Snider <MackSnider@users.noreply.github.com>
2021-01-30 16:34:32 -0500

    adding some js stuff

---
+++ js/main.js
@@ -0,0 +1,20 @@
+(() => {
+	// stub
+	console.log('Fired!')
+
+	let theIcon = document.querySelector('.wrapper img');
+		theIcons = document.querySelectorAll('.wrapper img')
+
+	let theIcons = document.querySelectorAll('.wrapper')
+
+	function logMe()
+		console.log('clicked the button!')
+
+
+def logClick:
+	print('clicked the button');
+}
+
+theButton.addEventListener('click', logMe);
+theIcon.forEach(icon => icon.addEventListener('click', logMe))
+})();
